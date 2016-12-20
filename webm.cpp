fc3ca12c04484d41c37e06988d2824500afc6051
Pavel Ershov <429573036@qip.ru>
2016-12-20 15:16:33 +0300

    New commentaries

--- webm.cpp
+++ webm.cpp
@@ -7,24 +7,26 @@
 #include <iostream>
 #include <fstream>
 #include <cassert>
+
 #define HAVE_STDINT_H 1
 extern "C" {
-#include "vpx_decoder.h"
-#include "vp8dx.h"
-#include "nestegg/nestegg.h"
+    #include "vpx_decoder.h"
+    #include "vp8dx.h"
+    #include "nestegg/nestegg.h"
 }
+
 #include <SDL/SDL.h>
 
 using namespace std;
+
+
 
 static unsigned int mem_get_le32(const unsigned char *mem) {
     return (mem[3] << 24)|(mem[2] << 16)|(mem[1] << 8)|(mem[0]);
 }
 
-
-void play_webm(char const* name);
-
-int ifstream_read(void *buffer, size_t size, void *context) {
+// читаем в буффер данные из потока
+int ifstream_read(void* buffer, size_t size, void* context) {
     ifstream* f = (ifstream*)context;
     f->read((char*)buffer, size);
     // success = 1
@@ -33,24 +35,29 @@ int ifstream_read(void *buffer, size_t size, void *context) {
     return f->gcount() == size ? 1 : f->eof() ? 0 : -1;
 }
 
-int ifstream_seek(int64_t n, int whence, void *context) {
+// смещаемся по потоку
+int ifstream_seek(int64_t n, int whence, void* context) {
     ifstream* f = (ifstream*)context;
     f->clear();
     ios_base::seekdir dir;
+
     switch (whence) {
-    case NESTEGG_SEEK_SET:
-        dir = fstream::beg;
-        break;
-    case NESTEGG_SEEK_CUR:
-        dir = fstream::cur;
-        break;
-    case NESTEGG_SEEK_END:
-        dir = fstream::end;
-        break;
+        case NESTEGG_SEEK_SET:{
+            dir = fstream::beg;
+        }break;
+        
+        case NESTEGG_SEEK_CUR:{
+            dir = fstream::cur;
+        }break;
+        
+        case NESTEGG_SEEK_END:{
+            dir = fstream::end;
+        }break;
     }
     f->seekg(n, dir);
-    if (!f->good())
+    if (!f->good()){
         return -1;
+    }
     return 0;
 }
 
@@ -88,48 +95,66 @@ void logger(nestegg * ctx, unsigned int severity, char const * fmt, ...) {
 }
 #endif
 
+// Запуск воспроизведения видео
 void play_webm(char const* name) {
-    int r = 0;
-    nestegg* ne;
+    nestegg* ne = NULL;
 
+    // поток чтения из файла
     ifstream infile(name);
+
     nestegg_io ne_io;
     ne_io.read = ifstream_read;
     ne_io.seek = ifstream_seek;
     ne_io.tell = ifstream_tell;
     ne_io.userdata = (void*)&infile;
 
-    r = nestegg_init(&ne, ne_io, NULL /* logger */, -1);
+    // открываем медиа-контейнер
+    int r = nestegg_init(&ne, ne_io, NULL /* logger */, -1);
     assert(r == 0);
 
+    // читаем длительность видео
     uint64_t duration = 0;
     r = nestegg_duration(ne, &duration);
     assert(r == 0);
     cout << "Duration: " << duration << endl;
 
+    // читаем длительность треков
     unsigned int ntracks = 0;
     r = nestegg_track_count(ne, &ntracks);
     assert(r == 0);
     cout << "Tracks: " << ntracks << endl;
 
+    // видео-параметры
     nestegg_video_params vparams;
     vparams.width = 0;
     vparams.height = 0;
 
-    vpx_codec_iface_t* interface;
+    vpx_codec_iface_t* interface = NULL;
     for (int i=0; i < ntracks; ++i) {
+        // получаем id кодека
         int id = nestegg_track_codec_id(ne, i);
         assert(id >= 0);
+
+        // тип трека
         int type = nestegg_track_type(ne, i);
         cout << "Track " << i << " codec id: " << id << " type " << type << " ";
-        interface = id == NESTEGG_CODEC_VP9 ? &vpx_codec_vp9_dx_algo : &vpx_codec_vp8_dx_algo;
+
+        // Определяем какой кодек будем использовать (указатель на функцию)
+        interface = (id == NESTEGG_CODEC_VP9) ? &vpx_codec_vp9_dx_algo : &vpx_codec_vp8_dx_algo;
+
+        // если у нас видео поток
         if (type == NESTEGG_TRACK_VIDEO) {
 
+            // получим параметры текущего потока
             r = nestegg_track_video_params(ne, i, &vparams);
             assert(r == 0);
+
+            // выводим информацию
             cout << vparams.width << "x" << vparams.height
                  << " (d: " << vparams.display_width << "x" << vparams.display_height << ")";
         }
+
+        // аудио поток
         if (type == NESTEGG_TRACK_AUDIO) {
             nestegg_audio_params params;
             r = nestegg_track_audio_params(ne, i, &params);
@@ -139,48 +164,60 @@ void play_webm(char const* name) {
         cout << endl;
     }
 
-    vpx_codec_ctx_t  codec;
-    int              flags = 0, frame_cnt = 0;
-    vpx_codec_err_t  res;
+    vpx_codec_ctx_t codec = 0;
+    int flags = 0; 
+    int frame_cnt = 0;
+    vpx_codec_err_t res = 0;
 
     cout << "Using " << vpx_codec_iface_name(interface) << endl;
-    /* Initialize codec */
+
+    // Инициализация кодека
     if(vpx_codec_dec_init(&codec, interface, NULL, flags)) {
         cerr << "Failed to initialize decoder" << endl;
         return;
     }
 
+
+    // Работа с выводом графики
     SDL_Surface* surface = 0;
     SDL_Overlay* overlay = 0;
-
 
 
     int video_count = 0;
     int audio_count = 0;
     nestegg_packet* packet = 0;
-    // 1 = keep calling
-    // 0 = eof
-    // -1 = error
-    while (1) {
-        r = nestegg_read_packet(ne, &packet);
-        if (r == 1 && packet == 0)
-            continue;
-        if (r <=0)
-            break;
 
+    while (1) {
+        // читаем пакет пока не прочитается
+        // 1 = keep calling
+        // 0 = eof
+        // -1 = error
+        r = nestegg_read_packet(ne, &packet);
+        if ((r == 1) && (packet == 0)){
+            continue;
+        }
+        if (r <=0){
+            break;
+        }
+
+        // получаем трек из пакета
         unsigned int track = 0;
         r = nestegg_packet_track(packet, &track);
         assert(r == 0);
 
         // TODO: workaround bug
+        // если трек-видео
         if (nestegg_track_type(ne, track) == NESTEGG_TRACK_VIDEO) {
             cout << "video frame: " << ++video_count << " ";
+
+            // количество пакетов
             unsigned int count = 0;
             r = nestegg_packet_count(packet, &count);
             assert(r == 0);
-            cout << "Count: " << count << " ";
-            int nframes = 0;
 
+            cout << "Count: " << count << " ";
+
+            int nframes = 0;
             for (int j=0; j < count; ++j) {
                 unsigned char* data;
                 size_t length;
